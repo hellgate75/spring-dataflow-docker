e36cb7f55821084e5aa8a68a2c70e60b83e325bd
Fabrizio Torelli <hellgate75@gmail.com>
2020-02-19 10:28:41 +0000

    Bugfix

--- spring-cloud-dataflow-server/create-dataflow-live-server-docker-image.sh
+++ spring-cloud-dataflow-server/create-dataflow-live-server-docker-image.sh
@@ -2,7 +2,7 @@
 if [ "" = "$SERVER_RELEASE" ]; then
 	SERVER_RELEASE="1.0.0"
 fi
-if [ "" = "$DOCKERHUB_USER" ]; then
-	DOCKERHUB_USER=hellgate75
+if [ "" = "$DOCKER_IMAGE_USER" ]; then
+	DOCKER_IMAGE_USER=hellgate75
 fi
-docker --debug image build --rm -f Dockerfile.live . -t $DOCKERHUB_USER/spring-cloud-dataflow-live-server:$SERVER_RELEASE
+docker --debug image build --rm -f Dockerfile.live . -t $DOCKER_IMAGE_USER/spring-cloud-dataflow-live-server:$SERVER_RELEASE
