241196ef5c2ac5d369eabe394030352ccdd3067b
Fabrizio Torelli <hellgate75@gmail.com>
2020-02-19 12:43:30 +0000

    improving scripts for Travis-CI

--- build-all.sh
+++ build-all.sh
@@ -18,10 +18,12 @@ UNAME="$(uname -a 2> /dev/null)"
 TRAVIS_CI="no"
 if [ "" != "$(echo $UNAME|grep Linux)" ]; then
 	## Into Travis-CI build
-	cd spring-cloud-config-server
+	sudo su -
+	cd $PWD/spring-cloud-config-server
 	unzip ssh.zip -d /root
 	chmod 600 /root/.ssh/*
 	cd $PWD
+	exit
 	cd ..
 	apt update && apt install -y maven
 	git clone git@github.com:hellgate75/dataflow-flow-centric-poc.git
