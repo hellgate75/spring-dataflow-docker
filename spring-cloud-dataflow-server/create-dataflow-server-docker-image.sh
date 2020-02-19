c2fb233d99718bd1786cba7cc0e668ca2d5b045f
Fabrizio Torelli <hellgate75@gmail.com>
2020-02-19 10:29:40 +0000

    Bugfix

--- spring-cloud-dataflow-server/create-dataflow-server-docker-image.sh
+++ spring-cloud-dataflow-server/create-dataflow-server-docker-image.sh
@@ -3,12 +3,12 @@ FOLDER="$(realpath "$(dirname "$0")")"
 if [ "" = "$SERVER_RELEASE" ]; then
 	SERVER_RELEASE="1.0.0"
 fi
-if [ "" = "$DOCKERHUB_USER" ]; then
-	DOCKERHUB_USER=hellgate75
+if [ "" = "$DOCKER_IMAGE_USER" ]; then
+	DOCKER_IMAGE_USER=hellgate75
 fi
 VARRGS=""
 for argument in $(cat $FOLDER/spring-cloud-dataflow-vars.env); do
   VARRGS="$VARRGS --build-arg $argument"
 done
 echo "Using arguments:$VARRGS"
-docker --debug image build $VARRGS --rm . -t $DOCKERHUB_USER/spring-cloud-dataflow-server:$SERVER_RELEASE
+docker --debug image build $VARRGS --rm . -t $DOCKER_IMAGE_USER/spring-cloud-dataflow-server:$SERVER_RELEASE
