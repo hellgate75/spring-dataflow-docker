f682e39238fe5505bcd96e91b8ba931a131b8859
Fabrizio Torelli <hellgate75@gmail.com>
2020-02-19 10:27:32 +0000

    Bugfix

--- spring-cloud-config-server/create-config-server-docker-image.sh
+++ spring-cloud-config-server/create-config-server-docker-image.sh
@@ -3,10 +3,11 @@
 if [ "" = "$CONFIG_SERVER_RELEASE" ]; then
 	CONFIG_SERVER_RELEASE="1.0.0"
 fi
-if [ "" = "$DOCKERHUB_USER" ]; then
-	DOCKERHUB_USER=hellgate75
+
+if [ "" = "$DOCKER_IMAGE_USER" ]; then
+	DOCKER_IMAGE_USER=hellgate75
 fi
 if [ ! -e ./dataflow-ms-config-server.jar ]; then
 	curl -sL https://ftorelli-software-compliance-repository.s3-eu-west-1.amazonaws.com/docker-machines/spring-dataflow/dataflow-ms-config-server.jar -o ./dataflow-ms-config-server.jar
 fi
-docker --debug image build --rm . -t $DOCKERHUB_USER/spring-cloud-config-server:$CONFIG_SERVER_RELEASE
+docker --debug image build --rm . -t $DOCKER_IMAGE_USER/spring-cloud-config-server:$CONFIG_SERVER_RELEASE
