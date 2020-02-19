e3c24a3a1b64fa2861ae619459e0129d1204607f
Fabrizio Torelli <hellgate75@gmail.com>
2020-02-19 03:02:55 +0000

    Adding arguments for normal SCDF and right param positions for live

--- spring-cloud-dataflow-server/create-dataflow-live-server-docker-image.sh
+++ spring-cloud-dataflow-server/create-dataflow-live-server-docker-image.sh
@@ -5,4 +5,4 @@ fi
 if [[ "" == "$DOCKERHUB_USER" ]]; then
 	DOCKERHUB_USER=hellgate75
 fi
-docker --debug image build --rm . -t $DOCKERHUB_USER/spring-cloud-dataflow-live-server:$SERVER_RELEASE Dockerfile.live
+docker --debug image build --rm -f Dockerfile.live . -t $DOCKERHUB_USER/spring-cloud-dataflow-live-server:$SERVER_RELEASE
--- spring-cloud-dataflow-server/create-dataflow-server-docker-image.sh
+++ spring-cloud-dataflow-server/create-dataflow-server-docker-image.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
+FOLDER="$(realpath "$(dirname "$0")")"
 if [[ "" == "$SERVER_RELEASE" ]]; then
 	SERVER_RELEASE="1.0.0"
 fi
 if [[ "" == "$DOCKERHUB_USER" ]]; then
 	DOCKERHUB_USER=hellgate75
 fi
-docker --debug image build --rm . -t $DOCKERHUB_USER/spring-cloud-dataflow-server:$SERVER_RELEASE
+VARRGS=""
+for argument in $(cat $FOLDER/spring-cloud-dataflow-vars.env); do
+  VARRGS="$VARRGS --build-arg $argument"
+done
+echo "Using arguments:$VARRGS"
+docker --debug image build $VARRGS --rm . -t $DOCKERHUB_USER/spring-cloud-dataflow-server:$SERVER_RELEASE
